80b397664aa1250627c397fefc9bd86f3fd86e49
Tecyle <tecyle@163.com>
2016-03-20 11:14:17 +0800

    First commit.

---
+++ public/includes/console_base.h
@@ -0,0 +1,34 @@
+#pragma once
+
+namespace Sanalar
+{
+	typedef ConsoleFontColor
+	{
+
+	} ConsoleFontColor;
+
+	typedef ConsoleBackgroundColor
+	{
+
+	} ConsoleBackgroundColor;
+
+	class Console
+	{
+	public:
+		// 基本输出功能
+		int printf(const char* format, ...);
+		int printf(const wchar_t* format, ...);
+		
+		void setStdHandle();
+
+		int printWithColor(ConsoleFontColor fontColor, ConsoleBackgroundColor backgroundColor, const char* format, ...);
+		int printWithColor(ConsoleFontColor fontColor, ConsoleBackgroundColor backgroundColor, const wchar_t* format, ...);
+
+		// 基本输入功能
+
+		// 控制台属性（对于非标准屏幕输出无效）
+		void hideCursor();
+		void showCursor();
+		bool isCursorVisiable() const;
+	};
+}
